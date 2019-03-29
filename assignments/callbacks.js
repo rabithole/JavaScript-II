6deec9de0b311b63dadf2be4eaae7afc84ba09be
Robert Kulp <pedalinon@gmail.com>
2019-03-29 13:56:33 -0700

    I think callback.js is done. I will return to the last one after I study more array methods.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,29 +1,29 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+ 
 
   //Given this problem: 
   
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+  // function firstItem(arr, cb) {
+  //   // firstItem passes the first item of the given array to the callback function.
+  // }
 
-  // Potential Solution:
+  // // Potential Solution:
 
-  // Higher order function using "cb" as the call back
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+  // // Higher order function using "cb" as the call back
+  // function firstItem(arr, cb) {
+  //   return cb(arr[0]);
+  // }
 
-  // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
+  // // Function invocation 
+  // firstItem(items, function(first) {
+  //   console.log(first)
+  // });
 
-*/
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+
+// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 ///////////////////////////////////////////
@@ -32,7 +32,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 //   return cb(arr.length);
 // }
 // getLength(items, (listLength) => {
-//   // console.log(listLength);
+//   console.log(listLength);
 // });
 
 
@@ -42,7 +42,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 //   return cb(arr.length - 1);
 // }
 // last(items, (lastItem) => {
-//   // console.log(items[lastItem]);
+//   console.log(items[lastItem]);
 // });
 
 
@@ -55,6 +55,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 //   // console.log(sum);
 // });
 
+// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 /////////////////////////////////////////////////////
 function multiplyNums(x, y, cb) {
@@ -62,7 +63,7 @@ function multiplyNums(x, y, cb) {
   return cb(x * y);
 }
 multiplyNums(10402, 239849484, (product) => {
-  console.log(`${product} ${'is a big number'}` );
+  console.log(`${product} is a big number`);
 });
 
 // const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
@@ -71,15 +72,17 @@ multiplyNums(10402, 239849484, (product) => {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  return cb();
+  return cb(item, list);
 }
-contains('Pencil', items, function(list) {
+contains('Pencil', items, (item, list) => {
   for(let i = 0; i < list.length; i++){
     if(list[i] === item){
-      return true;
+      console.log(true);
+    } else {
+      console.log(false)
     }
-    return false;
   }
+
 });
 
 /* STRETCH PROBLEM */
